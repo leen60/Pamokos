8d5dc348842b4cfcfd11cab4d66462a9b0ad6d35
laura <laura.zilinskaite@outlook.com>
2016-10-18 17:49:04 +0300

    3pam.

--- 2_pamoka_10_11/ShipBattle/ShipBattleTraining/Functions.cs
+++ 2_pamoka_10_11/ShipBattle/ShipBattleTraining/Functions.cs
@@ -1,10 +1,31 @@
-﻿namespace ShipBattleTraining
+﻿using System;
+
+namespace ShipBattleTraining
 {
     public static class Functions
     {
         public static ShotTarget SelectShotCoordinates(FieldData[][] battleField, ShotTarget[] previousTargets)
         {
-            return new ShotTarget { X = 0, Y = 0 };
+            int a = 0;
+            int b = 0;
+            ShotTarget suvis = new ShotTarget();
+            Random generator = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    a = generator.Next(0, 10);
+                    b = generator.Next(0, 10);
+                    if (battleField[a][b] == FieldData.StatusUnknonw)
+                    {
+                        suvis.X = a;
+                        suvis.Y = b;
+                    }
+                   
+     
+                }
+            }
+            return suvis;
         }
     }
 }
---
+++ 3Pamoka/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            decimal sum = 0;
+            string a = "value";
+
+
+            string variable = Console.ReadLine();
+            int count = int.Parse(variable);
+
+            decimal[] numbers = new decimal[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                string h = Console.ReadLine();
+                int d = int.Parse(h);
+                numbers[0] = d;
+
+            }
+            decimal vidurkis = CalculateAverage(numbers, count);
+            Console.WriteLine(vidurkis);
+            Console.ReadKey();
+        }
+
+        private static decimal CalculateAverage(decimal[] numbers, int count)
+        {
+            decimal suma = 0;
+            decimal vidurkis = 0;
+            for (int i = 0; i < count; i++)
+            {
+
+                suma += numbers[0];
+                 
+
+            }
+            vidurkis = suma / count;
+            return vidurkis;
+        }
+    }
+}
