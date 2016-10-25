0070ae0d759aefc0eabc2d99c08b723d3930cda0
laura <laura.zilinskaite@outlook.com>
2016-10-25 18:02:33 +0300

    4 pam.

--- 4_pamoka_10_25/FileIO/FileIO/Program.cs
+++ 4_pamoka_10_25/FileIO/FileIO/Program.cs
@@ -9,7 +9,7 @@ namespace FileIO
     {
         static void Main(string[] args)
         {
-            string input = ""; // nuskaityk failą į input kintamąjį
+            string input = File.ReadAllText(@"D:\SAUGYKLA\Laura Žilinskaitė\Pamokos\4_pamoka_10_25\FileIO\FileIO\longestNameInput.txt"); // nuskaityk failą į input kintamąjį
 
             FindSportsmenWithLongestFullname(input);
             FindSportsmenWithLongestFullnameLikeAPro(input);
@@ -19,6 +19,18 @@ namespace FileIO
 
         public static void FindSportsmenWithLongestFullname(string input)
         {
+            string longestn = "";
+            string[] longestname = input.Split(',');
+            for (int i = 0; i < longestname.Length; i++)
+            {
+                string currentname = longestname[i];
+                if (currentname.Length > longestn.Length)
+                {
+                    longestn = currentname;
+                }
+
+            }
+            File.WriteAllText(@"D:\SAUGYKLA\Laura Žilinskaitė\Pamokos\4_pamoka_10_25\FileIO\FileIO\longestNameV1.txt", longestn);
             //sukarpyk input į string[]
             //surask ilgiausią vardą
             //išvesk į failą longestNameV1.txt
@@ -26,6 +38,11 @@ namespace FileIO
 
         public static void FindSportsmenWithLongestFullnameLikeAPro(string input)
         {
+            string[] longestname = input.Split(',');
+            List<string> names = longestname.ToList();
+            String firstNameInList = names.First();
+            List<String> orderedList = names.OrderByDescending(x => x.Length).ToList();
+            File.WriteAllLines(@"D:\SAUGYKLA\Laura Žilinskaitė\Pamokos\4_pamoka_10_25\FileIO\FileIO\longestNameV2.txt", orderedList.ToArray());
             //sukarpyk input į List<string>
             //surask ilgiausią vardą
             //išvesk į failą longestNameV2.txt
@@ -33,6 +50,7 @@ namespace FileIO
         
         public static void SortSportsmenByFullnameLength(string input)
         {
+
             //sukarpyk input į List<string>
             //surūšiuok pagal vardo ilgį
             //išvesk į failą sortedNames.txt
