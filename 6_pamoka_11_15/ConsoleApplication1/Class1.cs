99762c1f832bc6eafd3fb6b8eeff011dd33c85c2
laura <laura.zilinskaite@outlook.com>
2016-11-15 18:04:15 +0200

    6 pamoka

---
+++ 6_pamoka_11_15/ConsoleApplication1/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApplication1
+{
+    public class Klase
+    {
+
+        private Dictionary<string, string> Temp;
+        public Klase()
+        {
+            string[] input = File.ReadAllLines(@"c:\users\mok\documents\visual studio 2015\Projects\ConsoleApplication3\ConsoleApplication1\chatBot.txt");
+            Temp = new Dictionary<string, string>();
+            foreach (var Element in input)
+            {
+                Temp.Add(Element.Split('~')[0], Element.Split('~')[1]);
+            }
+        }
+
+        public string metod(string x)
+        {
+            if (Temp.ContainsKey(x))
+            {
+                return Temp[x];
+            }
+            else
+                return "n";
+
+        }
+    }
+}
---
+++ 6_pamoka_11_15/ConsoleApplication1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Klase Temp = new Klase();
+            string j = Console.ReadLine();
+            
+            Console.WriteLine(Temp.metod(j));
+            Console.ReadKey();
+
+        }
+    }
+    
+}
