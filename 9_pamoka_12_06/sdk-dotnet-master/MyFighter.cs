1eb8984acab3dbab8a5537791f74e6e6129b181d
laura <laura.zilinskaite@outlook.com>
2016-12-06 17:32:03 +0200

    uzd

--- 9_pamoka_12_06/sdk-dotnet-master/MyFighter.cs
+++ 9_pamoka_12_06/sdk-dotnet-master/MyFighter.cs
@@ -10,7 +10,25 @@ namespace CodeFights
     {
         public Move MakeNextMove(Move opponentsLastMove, int myLastScore, int opponentsLastScore)
         {
-            return new Move();
+
+            Move jud = new Move();
+            Move judFalse = new Move();
+            if(opponentsLastMove == null)
+            {
+                return judFalse.AddAttack(Area.Groin).AddAttack(Area.Nose).AddDefence(Area.Jaw);
+
+            }
+
+            if(opponentsLastMove.Attacks.Contains(Area.Belly))
+            {
+                jud.AddDefence(Area.Belly);
+            }
+            else
+            {
+                jud.AddAttack(Area.Legs).AddAttack(Area.Belly).AddDefence(Area.Nose);
+            }
+            return jud;
+
         }
     }
 }
